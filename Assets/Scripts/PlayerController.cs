4f8d68dda1baa88778f1390ea2cefcd1efb5cab8
Negar <neg_r.saeidi95@yahoo.com>
2022-01-19 10:56:50 -0500

    Animation and blend tree added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    public bool isFiring;
+    public bool isReloading;
+    public bool isJumping;
+    public bool isRunning;
+
+
+
+}
